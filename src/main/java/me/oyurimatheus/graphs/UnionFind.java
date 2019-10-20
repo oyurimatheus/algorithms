5d9c71391ec17694682fcc9b99225ed7f4e76ba2
Yuri Matheus <yurimatheusb@hotmail.com>
2019-10-20 18:38:14 -0300

    creates union-find

---
+++ src/main/java/me/oyurimatheus/graphs/UnionFind.java
@@ -0,0 +1,46 @@
+package me.oyurimatheus.graphs;
+
+public class UnionFind {
+
+    private int[] elements;
+    private int[] size;
+
+    public UnionFind(int elements) {
+        this.elements = new int[elements];
+        this.size = new int[elements];
+
+        for(int i = 0; i < elements; i++) {
+            this.elements[i] = i;
+        }
+    }
+
+    public boolean connected(int p, int q) {
+        return elements[p] == elements[q];
+    }
+
+    public void union(int p, int q) {
+        int pElement = root(p);
+        int qElement = root(q);
+
+        if(pElement == qElement) {
+            return;
+        }
+
+        if(size[pElement] < size[qElement]) {
+            elements[pElement] = qElement;
+            size[qElement] += size[pElement];
+        } else {
+            elements[qElement] = pElement;
+            size[pElement] += size[qElement];
+        }
+    }
+
+    private int root(int i) {
+        while(i != elements[i]) {
+            elements[i] = elements[elements[i]];
+            i = elements[i];
+        }
+
+        return i;
+    }
+}
---
+++ src/test/java/me/oyurimatheus/graphs/UnionFindTest.java
@@ -0,0 +1,29 @@
+package me.oyurimatheus.graphs;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class UnionFindTest {
+
+    @Test
+    void shouldConnectTheElementsWhenRequired() {
+        UnionFind uf = new UnionFind(10);
+
+        uf.union(0, 1);
+        uf.union(1, 2);
+        uf.union(3, 4);
+        uf.union(9, 8);
+
+        assertThat(uf.connected(0, 1)).isTrue();
+        assertThat(uf.connected(1, 2)).isTrue();
+        assertThat(uf.connected(0, 2)).isTrue();
+        assertThat(uf.connected(4, 3)).isTrue();
+        assertThat(uf.connected(8, 9)).isTrue();
+
+        assertThat(uf.connected(3, 1)).isFalse();
+        assertThat(uf.connected(0, 9)).isFalse();
+        assertThat(uf.connected(6, 7)).isFalse();
+    }
+
+}
